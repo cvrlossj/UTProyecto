670784f0069c6921b371591ba40ead02f172254d
Carlos Silva <67534566+cvrlossj@users.noreply.github.com>
2024-11-15 23:43:42 -0300

    Se Agrego mejoras en los acceso para cada perfil

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
-from django.views.generic import View
+from django.urls import reverse
+from django.shortcuts import redirect
 from django.contrib.auth.views import LoginView
 from .models import Perfiles
-
+from django.contrib.auth import logout
+from django.views.generic import View
 
 # Esto es una vista personalizada de Incio de Sesión
 class CustomLoginView(LoginView):
-    def get_succes_url(self):
+    def get_success_url(self):
         user = self.request.user
+        
+        if user.id_estadoperfil and user.id_estadoperfil.descripcion != "habilitado":
+            return reverse("accounts:login")
+        
         if user.id_rol.descripcion == "Administrador":
-            pass
-        elif user.id_rol.descripcion == "Junta de Vecino":
+            return reverse('dashboarda:superadmin') 
+        elif user.id_rol.descripcion == "Junta Vecinos":
             pass
         elif user.id_rol.descripcion == "Vecino":
             pass
-        else:
-            pass
+        
+        return reverse('accounts:login')
+    
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect("accounts:login")
