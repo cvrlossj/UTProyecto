979151b9ce31ed2edbf4c7615949ec39e59f9342
Carlos Silva <67534566+cvrlossj@users.noreply.github.com>
2024-11-25 07:19:13 -0300

    chore(dashboardjv): Se agregan las señales para migraciones

---
+++ dashboardjv/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from .models import EstadoNoticia, EstadoActividad, EstadoCertificado, EstadoPostulacion
+
+@receiver(post_migrate)
+def create_default_data(sender, **kwargs):
+     if sender.name == 'dashboardjv':
+         EstadoNoticia.objects.get_or_create(id_estadonoticia=1, descripcion='habilitada')
+         EstadoNoticia.objects.get_or_create(id_estadonoticia=2, descripcion='inhabilitada')
+         
+         EstadoActividad.objects.get_or_create(id_estadoactividad=1, descripcion='habilitada')
+         EstadoActividad.objects.get_or_create(id_estadoactividad=2, descripcion='inhabilitada')
+         
+         EstadoCertificado.objects.get_or_create(id_estadocertificado=1, descripcion='pendiente')
+         EstadoCertificado.objects.get_or_create(id_estadocertificado=2, descripcion='emitido')
+         
+         EstadoPostulacion.objects.get_or_create(id_estadopostulacion=1, descripcion='pendiente')
+         EstadoPostulacion.objects.get_or_create(id_estadopostulacion=2, descripcion='aprobado')
+         EstadoPostulacion.objects.get_or_create(id_estadopostulacion=3, descripcion='rechazado')
