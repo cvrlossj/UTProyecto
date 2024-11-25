56b071bde6a7b8238c0a3836ab0f0d1dfac0d4ab
Carlos Silva <67534566+cvrlossj@users.noreply.github.com>
2024-11-25 07:22:22 -0300

    fix: Se agregan nuevos estados al Estado Certificado (aprobar, rechazar)

--- dashboardjv/signals.py
+++ dashboardjv/signals.py
@@ -4,16 +4,18 @@ from .models import EstadoNoticia, EstadoActividad, EstadoCertificado, EstadoPos
 
 @receiver(post_migrate)
 def create_default_data(sender, **kwargs):
-     if sender.name == 'dashboardjv':
-         EstadoNoticia.objects.get_or_create(id_estadonoticia=1, descripcion='habilitada')
-         EstadoNoticia.objects.get_or_create(id_estadonoticia=2, descripcion='inhabilitada')
-         
-         EstadoActividad.objects.get_or_create(id_estadoactividad=1, descripcion='habilitada')
-         EstadoActividad.objects.get_or_create(id_estadoactividad=2, descripcion='inhabilitada')
-         
-         EstadoCertificado.objects.get_or_create(id_estadocertificado=1, descripcion='pendiente')
-         EstadoCertificado.objects.get_or_create(id_estadocertificado=2, descripcion='emitido')
-         
-         EstadoPostulacion.objects.get_or_create(id_estadopostulacion=1, descripcion='pendiente')
-         EstadoPostulacion.objects.get_or_create(id_estadopostulacion=2, descripcion='aprobado')
-         EstadoPostulacion.objects.get_or_create(id_estadopostulacion=3, descripcion='rechazado')
+    if sender.name == 'dashboardjv':
+            EstadoNoticia.objects.get_or_create(id_estadonoticia=1, descripcion='habilitada')
+            EstadoNoticia.objects.get_or_create(id_estadonoticia=2, descripcion='inhabilitada')
+            
+            EstadoActividad.objects.get_or_create(id_estadoactividad=1, descripcion='habilitada')
+            EstadoActividad.objects.get_or_create(id_estadoactividad=2, descripcion='inhabilitada')
+            
+            EstadoCertificado.objects.get_or_create(id_estadocertificado=1, descripcion='pendiente')
+            EstadoCertificado.objects.get_or_create(id_estadocertificado=2, descripcion='emitido')
+            EstadoCertificado.objects.get_or_create(id_estadocertificado=3, descripcion='aprobar')
+            EstadoCertificado.objects.get_or_create(id_estadocertificado=4, descripcion='rechazar')
+            
+            EstadoPostulacion.objects.get_or_create(id_estadopostulacion=1, descripcion='pendiente')
+            EstadoPostulacion.objects.get_or_create(id_estadopostulacion=2, descripcion='aprobado')
+            EstadoPostulacion.objects.get_or_create(id_estadopostulacion=3, descripcion='rechazado')
