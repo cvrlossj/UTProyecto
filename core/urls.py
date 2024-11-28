7d2452f367e5762aa9d257c53d136b7ff8eb5c79
Carlos Silva <67534566+cvrlossj@users.noreply.github.com>
2024-11-28 01:02:13 -0300

    fix: se integra un redirect al login

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Redirect to accounts
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
     # Accounts View's
     path('accounts/', include('accounts.urls')),
     # Dashboard Admin
